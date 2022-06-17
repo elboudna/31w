4b3e41f55a937c07cd5ad3c5d882acd9112ba07a
MehdiB <medi.mtfi@gmail.com>
2022-06-17 08:27:13 -0400

    ajout de plugin p1

---
+++ wp-content/plugins/short_2/script/message.js
@@ -0,0 +1,13 @@
+(function(){
+
+    let boite__modale = document.createElement('div');
+    elmBoite__modale.classList.add('boite__modale');
+
+
+    document.querySelector('.boite__modale')
+    let elmOK = document.querySelector('#mon_bouton')
+
+    elmOK.addEventListener('mousedown',function(){
+        console.log('Bravo le script fonctionne')
+    })
+})
--- wp-content/plugins/short_2/short_2.php
+++ wp-content/plugins/short_2/short_2.php
@@ -13,12 +13,12 @@ function sh2_31w_enqueue()
     $version_js = filemtime(plugin_dir_path(__FILE__) . 'script/message.js');
 
     wp_enqueue_style(   'sh2_31w_css',
-                        plugin_dir_url(__FILE__, "/style.css"),
+                        plugin_dir_url(__FILE__). "style.css",
                         array(),
                         $version_css);
     
     wp_enqueue_script(   'sh2_31w_js',
-                        plugin_dir_url(__FILE__, "/script/message.js"),
+                        plugin_dir_url(__FILE__)."script/message.js",
                         array(),
                         $version_js, true);
 
